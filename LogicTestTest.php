999ff882a83ff45c0929a6e70de20f3335648c19
Steve Torrens <stevetorrens70@gmail.com>
2018-12-10 16:48:44 +1300

    working on logic test

---
+++ LogicTestTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class logicTestTest extends TestCase
+{
+  public function testSomething() {
+    $logic_test = new LogicTest();
+    $logic_test->process_numbers();
+
+
+  }
+
+}
