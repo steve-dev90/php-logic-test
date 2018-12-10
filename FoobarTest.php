d7c49f5a3a6ff153cdf68231894e6a6b8f4a78f5
Steve Torrens <stevetorrens70@gmail.com>
2018-12-11 10:27:55 +1300

    second cut complete

--- FoobarTest.php
+++ FoobarTest.php
@@ -4,12 +4,15 @@ require './foobar.php';
 
 class FoobarTest extends TestCase
 {
-  public function testSomething() {
-    echo "\n\n ******GHG***** \n\n";
+  public function testGetResult() {
     $foobar = new Foobar();
-    echo $foobar->get_result(5);
-
-
+    $this->assertSame('foo', $foobar->get_result(3));
+    $this->assertSame('foo', $foobar->get_result(18));
+    $this->assertSame('bar', $foobar->get_result(5));
+    $this->assertSame('bar', $foobar->get_result(35));
+    $this->assertSame('foobar', $foobar->get_result(15));
+    $this->assertSame('foobar', $foobar->get_result(90));
+    $this->assertSame('4', $foobar->get_result(4));
+    $this->assertSame('16', $foobar->get_result(16));
   }
-
 }
